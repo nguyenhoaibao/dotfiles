74c4fdfc27898b76f336a31fa3ce2c5b3ca295d4
nguyenhoaibao <nhbao.la@gmail.com>
2022-05-23 13:20:37 +0700

    Add init.lua

--- install-scripts/linux/install.sh
+++ install-scripts/linux/install.sh
@@ -1,79 +1,74 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y zsh
-sudo apt-get install -y silversearcher-ag ripgrep
-sudo apt-get install -y git wget direnv tmux alacritty fonts-firacode
-sudo apt-get install -y xclip
-sudo apt-get install -y universal-ctags
+sudo apt-get install -y zsh ca-certificates curl gnupg silversearcher-ag ripgrep \
+    git wget direnv tmux fonts-firacode fzf \
+    xclip universal-ctags jq lsb-release apt-transport-https \
+    make binutils bison gcc build-essential
 
 # install neovim
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt-get update -y
-sudo apt-get install -y neovim
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-pip2 install neovim
-pip3 install neovim
+sudo apt-get install -y python-dev python3-dev python3-pip python3-neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+sudo install -o root -g root -m 0755 nvim.appimage /usr/local/bin/nvim.appimage
+pip3 install --user --upgrade pynvim msgpack
 
-# install NodeJS
-bash -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh)"
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.1/install.sh | bash
 . $HOME/.nvm/nvm.sh
 nvm install stable
 nvm alias default node
-npm install --global prettier neovim
+npm install --global prettier neovim yarn
 
-# install Go
-goversion="1.16"
-goos="linux"
-goarch="amd64"
-goname="go$goversion.$goos-$goarch.tar.gz"
-godl="https://storage.googleapis.com/golang/$goname"
+# install gvm
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+. $HOME/.gvm/scripts/gvm
+gvm install go1.17 -B
+gvm use go1.17 --default
 
-wget $godl
-sudo tar -C /usr/local -xvzf $goname
-rm -f $goname
+# rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install Docker
-# See: https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt-get remove docker docker-engine docker.io containerd runc
-DOCKER_DOWNLOAD_URL=https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64
-
-urls=(
-    "$DOCKER_DOWNLOAD_URL/docker-ce_19.03.0~3-0~ubuntu-bionic_amd64.deb"
-    "$DOCKER_DOWNLOAD_URL/docker-ce-cli_19.03.0~3-0~ubuntu-bionic_amd64.deb"
-    "$DOCKER_DOWNLOAD_URL/containerd.io_1.2.6-3_amd64.deb"
-)
-
-for url in "${urls[@]}"
-do
-    TMP_DEB="$(mktemp)" && wget -O "$TMP_DEB" "$url" && sudo dpkg -i "$TMP_DEB" && rm -f "$TMP_DEB"
-done
+curl -fsSL https://get.docker.com | sh
 sudo groupadd docker
 sudo usermod -aG docker $USER
-# install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
 echo -n "Install kubectl and helm? (y/N) => "; read cp
 if [[ $cp == "y" ]]; then
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    sudo mv ./kubectl /usr/local/bin/kubectl
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
 
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-    BINARY_NAME=helm3 bash get_helm.sh
-    rm -f ./get_helm.sh
+    curl -LO https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
+    tar xzf helm-v3.8.0-linux-amd64.tar.gz
+    sudo mv linux-amd64/helm /usr/local/bin/helm
+    rm -f helm-v3.8.0-linux-amd64.tar.gz
+    rm -rf linux-amd64
+
+    curl -LO https://github.com/derailed/k9s/releases/download/v0.25.18/k9s_Linux_x86_64.tar.gz
+    tar xzf k9s_Linux_x86_64.tar.gz
+    sudo mv k9s /usr/local/bin/k9s
+    rm -f k9s_Linux_x86_64.tar.gz
+    rm -f README.md
+    rm -f LICENSE
 fi
 
 echo -n "Install gcloud? (y/N) => "; read cp
 if [[ $cp == "y" ]]; then
-    # See https://cloud.google.com/sdk/docs/downloads-apt-get
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    sudo apt-get install apt-transport-https ca-certificates gnupg
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    sudo apt-get update && sudo apt-get install google-cloud-sdk
+    curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-371.0.0-linux-x86_64.tar.gz
+    mkdir -p ~/.zsh
+    tar xzf google-cloud-sdk-371.0.0-linux-x86_64.tar.gz -C ~/.zsh
+    ~/.zsh/google-cloud-sdk/install.sh --quiet \
+        --usage-reporting false \
+        --path-update false \
+        --command-completion false
 fi
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo -n "Copy dotfiles to local? (y/N) => "; read cp
 if [[ $cp == "y" ]] || [[ $cp == "Y" ]] ; then
@@ -91,19 +86,21 @@ if [[ $cp == "y" ]] || [[ $cp == "Y" ]] ; then
         mv ~/.vimrc ~/.vimrc.$(date +%s)
     fi
 
-    echo "Copying dotfiles"
-    cp zshrc ~/.zshrc
-    cp tmux.linux.conf ~/.tmux.conf
+    echo "Copying dotfiles..."
+    mkdir -p ~/.config/alacritty
     cp alacritty.yml ~/.config/alacritty/alacritty.yml
-    mkdir -p ~/.config
-    cp -R nvim ~/.config
+    cp -R nvim/init.vim ~/.config/nvim/init.vim
+
+    mkdir -p ~/.zsh/functions
+    cp zshrc ~/.zshrc
+    cp zsh/functions/fbr ~/.zsh/functions/fbr
+    cp tmux.linux.conf ~/.tmux.conf
 fi
 
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mkdir -p ~/.tmux
 
 # install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# kube-tmux
+git clone https://github.com/jonmosco/kube-tmux.git ~/.tmux/kube-tmux
--- zshrc
+++ zshrc
@@ -10,20 +10,25 @@ source <(helm completion zsh)
 
 alias vim="nvim.appimage"
 alias nvim="nvim.appimage"
-alias tx="tmuxinator"
 alias t="terraform"
 if [ -f ~/.zsh/alias ]; then
   source ~/.zsh/alias
 fi
 
 export EDITOR="/usr/local/bin/nvim.appimage"
-export GOPATH="$HOME/go"
-export GOROOT="/usr/local/go"
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/snap/bin"
-export PATH=$PATH:"$GOROOT/bin":"$GOPATH/bin"
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.local/share/solana/install/active_release/bin"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+
+export GVM_DIR="$HOME/.gvm"
+[[ -s "$GVM_DIR/scripts/gvm" ]] && source "$GVM_DIR/scripts/gvm"
+
+export GCLOUD_SDK_DIR="$HOME/.zsh/google-cloud-sdk"
+[[ -s "$GCLOUD_SDK_DIR/completion.zsh.inc" ]] && source "$GCLOUD_SDK_DIR/completion.zsh.inc"
+[[ -s "$GCLOUD_SDK_DIR/path.zsh.inc" ]] && source "$GCLOUD_SDK_DIR/path.zsh.inc"
 
 bindkey '^ ' autosuggest-accept
 
@@ -33,11 +38,9 @@ export FZF_DEFAULT_OPTS='--bind=ctrl-f:preview-down,ctrl-b:preview-up'
 
 eval "$(direnv hook zsh)"
 
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
 fpath=(
   ~/.zsh/functions
   $fpath
 )
 autoload -Uz $fpath[1]/*(.:t)
+autoload -Uz compinit && compinit
